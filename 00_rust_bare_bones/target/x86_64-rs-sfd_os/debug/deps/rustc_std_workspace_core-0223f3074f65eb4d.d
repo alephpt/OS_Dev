c0f9e75020f3d78078fdad18658f7d92a7ef727d
alephpt <alephpt@github.com>
2024-04-29 15:35:09 -0500

    restructuring a bit

--- 00_rust_bare_bones/target/x86_64-rs-sfd_os/debug/deps/rustc_std_workspace_core-0223f3074f65eb4d.d
+++
@@ -1,7 +0,0 @@
-/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS_Dev/sfdOS-rs/target/x86_64-rs-sfd_os/debug/deps/librustc_std_workspace_core-0223f3074f65eb4d.rmeta: /home/persist/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS_Dev/sfdOS-rs/target/x86_64-rs-sfd_os/debug/deps/librustc_std_workspace_core-0223f3074f65eb4d.rlib: /home/persist/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS_Dev/sfdOS-rs/target/x86_64-rs-sfd_os/debug/deps/rustc_std_workspace_core-0223f3074f65eb4d.d: /home/persist/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs
-
-/home/persist/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/rustc-std-workspace-core/lib.rs:
