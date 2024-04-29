1bb0218f8d202ff5a2190b62a38350140f312100
alephpt <alephpt@github.com>
2024-04-29 13:59:02 -0500

    merging old and new OS Dev Projects

---
+++ sfdOS-rs/target/debug/build/bootloader-fec48d358ab23f90/build_script_build-fec48d358ab23f90.d
@@ -0,0 +1,5 @@
+/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS/sfdOS-rs/target/debug/build/bootloader-fec48d358ab23f90/build_script_build-fec48d358ab23f90: /home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.12/build.rs
+
+/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS/sfdOS-rs/target/debug/build/bootloader-fec48d358ab23f90/build_script_build-fec48d358ab23f90.d: /home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.12/build.rs
+
+/home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bootloader-0.9.12/build.rs:
---
+++ sfdOS-rs/target/debug/build/compiler_builtins-fc32ac8f2fe27923/build_script_build-fc32ac8f2fe27923.d
@@ -0,0 +1,5 @@
+/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS/sfdOS-rs/target/debug/build/compiler_builtins-fc32ac8f2fe27923/build_script_build-fc32ac8f2fe27923: /home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.108/build.rs
+
+/home/persist/z/Ancillary/Big Stick Studios/repos/learning/OS/sfdOS-rs/target/debug/build/compiler_builtins-fc32ac8f2fe27923/build_script_build-fc32ac8f2fe27923.d: /home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.108/build.rs
+
+/home/persist/.cargo/registry/src/index.crates.io-6f17d22bba15001f/compiler_builtins-0.1.108/build.rs:
